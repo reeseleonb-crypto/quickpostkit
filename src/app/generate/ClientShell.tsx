c17a684393e651aa111cf134f07563a0478d014d
Reese <reeseleonb@gmail.com>
2025-10-23 05:32:38 -0400

    ClientShell: render GenerateForm (replace placeholder)

--- src/app/generate/ClientShell.tsx
+++ src/app/generate/ClientShell.tsx
@@ -1,10 +1,7 @@
 "use client";
 
+import GenerateForm from "./GenerateForm";
+
 export default function ClientShell() {
-  return (
-    <main style={{ minHeight: "60vh", padding: 24 }}>
-      <h1>Generate – live page</h1>
-      <p>If you can see this, the route is now rendering ClientShell.</p>
-    </main>
-  );
+  return <GenerateForm />;
 }
